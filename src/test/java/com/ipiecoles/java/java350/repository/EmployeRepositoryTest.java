1d22ee6439f036b19dd7a1d076fced41b9aea6da
Alexis Cabrol <unconfigured@null.spigotmc.org>
2020-03-05 09:25:35 +0100

    Ajout de la correction pour EmployeRepositoryTest sur les tests paramétrés

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,20 +1,21 @@
 package com.ipiecoles.java.java350.repository;
 
 import com.ipiecoles.java.java350.model.Employe;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-@DataJpaTest
+@SpringBootTest
 class EmployeRepositoryTest {
 
     @Autowired
-    EmployeRepository employeRepository;
+    private EmployeRepository employeRepository;
 
     @BeforeEach
     @AfterEach
@@ -23,16 +24,34 @@ class EmployeRepositoryTest {
     }
 
     @Test
-    public void testFindByImmat() {
-        // Given
-        Employe employe = new Employe();
-        employe.setMatricule("TESTMAT");
-        employeRepository.save(employe);
+    void findLastMatriculeNull() {
+        //Given
 
-        // When
-        String findLastMatriculeResult = employeRepository.findLastMatricule();
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
 
-        // Then
-        Assertions.assertThat(findLastMatriculeResult).isEqualTo("ESTMAT");
+        //Then
+        Assertions.assertNull(lastMatricule);
+    }
+
+    @Test
+    void findLastMatriculeNominal() {
+        //Given
+        employeRepository.save(new Employe("Doe","John","C12345", LocalDate.now(),1500.0,1,1.0));
+        employeRepository.save(new Employe("Doe","John","T02345", LocalDate.now(),1500.0,1,1.0));
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+
+        //Then
+        Assertions.assertEquals("12345",lastMatricule);
+    }
+
+    @Test
+    void findByMatricule() {
+    }
+
+    @Test
+    void avgPerformanceWhereMatriculeStartsWith() {
     }
 }
